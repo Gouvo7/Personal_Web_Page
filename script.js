ca5dbbd5d77050683207ceeb0eb5deb1d418e70b
gouvo7 <gouvousisnektarios@gmail.com>
2024-12-25 14:10:54 +0200

    Added transitions (need to beautify them)

--- script.js
+++ script.js
@@ -1,47 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const typingTextElements = document.getElementsByClassName("typing-text");
-  let currentElementIndex = 0;
-  let currentTextIndex = 0;
-
-  function type() {
-    if (currentElementIndex < typingTextElements.length) {
-      const currentElement = typingTextElements[currentElementIndex];
-      const text = currentElement.dataset.text;
-
-      if (currentTextIndex < text.length) {
-        const char = text[currentTextIndex];
-        currentElement.innerHTML += char === '\n' ? '<br>' : char;
-        currentTextIndex++;
-        setTimeout(type, 50); // Adjust typing speed here
-      } else {
-        // Move to next element
-        currentElementIndex++;
-        currentTextIndex = 0;
-        setTimeout(type, 1000); // Delay before starting the next element
-      }
-    }
-  }
-
-  // Store the text content in a data attribute and clear the initial text content
-  for (let element of typingTextElements) {
-    element.dataset.text = element.textContent;
-    element.textContent = '';
-  }
-
-  // Start typing for the first element
-  type();
-});
-
 function scrollToSection(sectionId) {
-  document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+  document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
 }
 
 var scrollEnabled = false;
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const getStartedSection = document.querySelector(".get_started");
 
-  getStartedSection.addEventListener("click", function() {
-      document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+  getStartedSection.addEventListener("click", function () {
+    document.getElementById("about").scrollIntoView({ behavior: "smooth" });
   });
 });
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+      observer.unobserve(entry.target);
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElementsLeft = document.querySelectorAll(".hidden_left");
+hiddenElementsLeft.forEach((el) => observer.observe(el));
+
+const hiddenElementsRight = document.querySelectorAll(".hidden_right");
+hiddenElementsRight.forEach((el) => observer.observe(el));
